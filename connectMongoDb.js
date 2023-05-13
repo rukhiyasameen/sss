cae52e3f5efc7d0b8659bfa6be158bf008c00511
rukhiyasameen <119377489+rukhiyasameen@users.noreply.github.com>
2023-05-13 05:57:31 -0700

    Update connectMongoDb.js

--- connectMongoDb.js
+++ connectMongoDb.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(  "mongodb+srv://smrukhiyasameena:anjuman@cluster0.sznrkjg.mongodb.net/?retryWrites=true&w=majority";
-, {
+    const conn = await mongoose.connect(  "mongodb+srv://smrukhiyasameena:anjuman@cluster0.sznrkjg.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
